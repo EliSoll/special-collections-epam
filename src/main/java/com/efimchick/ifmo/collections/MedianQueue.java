e4acd3aa081cf1f15f92b7c07899decc921c0f47
Elvira Solnyshkina <elvira.solnyshkina@gmail.com>
2022-09-16 21:18:31 +0300

    add MedianQueue implementation: methods(peek(); poll();

--- src/main/java/com/efimchick/ifmo/collections/MedianQueue.java
+++ src/main/java/com/efimchick/ifmo/collections/MedianQueue.java
@@ -1,13 +1,8 @@
 package com.efimchick.ifmo.collections;
 
-/* offer - push element to the queue
-poll - pull element out of the queue
-peek - get element on the top of the queue (just get, no pulling out)
-iterator - iterate over elements of the queue (no need to keep order)
-size - just amount of current queue elements*/
-import java.sql.Array;
+
 import java.util.*;
-import java.util.function.Consumer;
+
 
 
 class MedianQueue extends PriorityQueue<Integer> {
